f1ff96f2f2a2a7583c9b4321bb5e71274d4f94f9
ihar.karunny <ihar.karunny@viber.com>
2024-02-16 19:16:50 +0300

    Project added

---
+++ YOU360/YOU360/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YOU360
+//
+//  Created by Ihar Karunny on 2/16/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
