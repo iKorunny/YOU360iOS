9ce8d385def779ec5c345aa0147c010b9b6ee810
ihar.karunny <ihar.karunny@viber.com>
2024-02-21 11:45:48 +0300

    NavigationBar done for login

--- YOU360/YOUAuthorization/Screens/LoginMainVC.swift
+++ YOU360/YOUAuthorization/Screens/LoginMainVC.swift
@@ -13,18 +13,6 @@ class LoginMainVC: CustomNavigationViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        customNavigationBar.set(title: "ButtonLoginTitle".localised())
+        title = "ButtonLoginTitle".localised()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- YOU360/YOUUIComponents/CustomNavigationViewController.swift
+++ YOU360/YOUUIComponents/CustomNavigationViewController.swift
@@ -12,37 +12,53 @@ open class CustomNavigationViewController: UIViewController {
     private enum Constants {
         static let barHeight: CGFloat = 44
         static let barTopOffset: CGFloat = 52
+        
+        static let logoViewSize = CGSize(width: 75, height: 25)
+        static let logoInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 18)
+        
+        static let titleFontSize: CGFloat = 18
     }
     
-    public lazy var customNavigationBar: CustomNavigationBar = {
-        let bar = CustomNavigationBar()
-        bar.translatesAutoresizingMaskIntoConstraints = false
-        bar.backgroundColor = .clear
-        return bar
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: .init(named: "LOGOYOU360"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalToConstant: Constants.logoViewSize.height).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: Constants.logoViewSize.width).isActive = true
+        return imageView
     }()
 
     open override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.isNavigationBarHidden = false
         view.backgroundColor = ColorPallete.appWhite
         setupUI()
+        
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor : ColorPallete.appBlackSecondary,
+            NSAttributedString.Key.font : YOUFontsProvider.appSemiBoldFont(with: Constants.titleFontSize)
+        ]
+
+        let backBTN = UIBarButtonItem(image: UIImage(named: "NavigationBack"),
+                                      style: .plain,
+                                      target: navigationController,
+                                      action: #selector(UINavigationController.popViewController(animated:)))
+        navigationItem.leftBarButtonItem = backBTN
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
     
     private func setupUI() {
-        view.addSubview(customNavigationBar)
-        customNavigationBar.heightAnchor.constraint(equalToConstant: Constants.barHeight).isActive = true
-        customNavigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        customNavigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        customNavigationBar.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.barTopOffset).isActive = true
-        customNavigationBar.setBackButton(target: self, action: #selector(popBack), for: .touchUpInside)
-        
-        customNavigationBar.setBackButton(hidden: true)
-        guard let navigationController = navigationController else { return }
-        customNavigationBar.setBackButton(hidden: navigationController.viewControllers.first === self)
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        navigationBar.addSubview(logoImageView)
+        logoImageView.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor, constant: -Constants.logoInsets.right).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: navigationBar.centerYAnchor).isActive = true
     }
     
     @objc private func popBack() {
         navigationController?.popViewController(animated: true)
     }
+}
+
+extension CustomNavigationViewController: UIGestureRecognizerDelegate {
+    
 }
